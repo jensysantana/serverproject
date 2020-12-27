850fbbee51b44c459546d63218f611f943e49a6c
JDaniel <jensysantana@gmail.com>
2020-12-26 23:00:14 -0500

    ready to upload to heroku

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
 process.env.httpServer = 4000;
 // process.env.httpsServer = process.env.PORT || 3443;
 process.env.BACKENDURL = '';
+//https://git.heroku.com/server-users.git
 process.env.BACKENDLOCALURL = 'mongodb://localhost:27017/fakerusersflutter';
